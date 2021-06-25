3e5479fd1879df694f6060127353278e7f800f06
Tre Babcock <tre.babcock@gmail.com>
2021-06-25 18:08:51 -0500

    fixed style issue for real this time

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [
     "./src/views/**/*.{js,ts,jsx,tsx,vue}",
     "./src/components/**/*.{js,ts,jsx,tsx,vue}",
+    ".src/*.{js,ts,jsx,tsx,vue}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
